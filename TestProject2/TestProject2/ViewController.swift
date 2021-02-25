1163b03cb48c893deeadee2ff22834fb04b2ea88
orknytr-fb <78570796+orknytr-fb@users.noreply.github.com>
2021-02-26 02:15:41 +0300

    first commit

---
+++ TestProject2/TestProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject2
+//
+//  Created by Orkun on 26.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("xxx")
+    }
+
+
+}
+
