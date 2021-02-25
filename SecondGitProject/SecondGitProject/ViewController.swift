dcd344116354688bc4fa6611414450bf8d1102df
orknytr-fb <78570796+orknytr-fb@users.noreply.github.com>
2021-02-26 00:36:47 +0300

    second upload complete

---
+++ SecondGitProject/SecondGitProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SecondGitProject
+//
+//  Created by Orkun on 26.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        print("second gidiyor")
+
+    }
+
+
+}
+
