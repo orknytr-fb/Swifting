422c3656f456dc6aab8cb17be6aa7cd01fa7bab9
orknytr-fb <78570796+orknytr-fb@users.noreply.github.com>
2021-02-26 01:06:22 +0300

    final geliyor

---
+++ ThirdProject/ThirdProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThirdProject
+//
+//  Created by Orkun on 26.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         print ("third geliyor")
+    }
+
+
+}
+
