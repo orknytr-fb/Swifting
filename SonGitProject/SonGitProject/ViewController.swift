4b5c970736e28ed1bc822b2e4d0bd98d79137d1d
orknytr-fb <78570796+orknytr-fb@users.noreply.github.com>
2021-02-26 01:49:53 +0300

    last update

---
+++ SonGitProject/SonGitProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SonGitProject
+//
+//  Created by Orkun on 26.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("orkun yatar")
+        
+    }
+
+
+}
+
